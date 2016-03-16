d2a065226fda1482736258da18d5a4e422c8ec6a
ballza1150 <ballzacup1@gmail.com>
2016-03-16 08:47:06 +0700

    Intent to singUp

---
+++ app/src/main/java/nuttapon/ball/resortball/SingUpActiviity.java
@@ -0,0 +1,13 @@
+package nuttapon.ball.resortball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingUpActiviity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up_activiity);
+    }
+}
