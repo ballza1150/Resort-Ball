856906d91b66299f5aebbb53a1a2fbe519205d13
ballza1150 <ballzacup1@gmail.com>
2016-03-31 17:01:22 +0700

    finish 31 march

--- app/src/main/java/nuttapon/ball/resortball/ShowServiceActivity.java
+++ app/src/main/java/nuttapon/ball/resortball/ShowServiceActivity.java
@@ -2,12 +2,41 @@ package nuttapon.ball.resortball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ShowServiceActivity extends AppCompatActivity {
+
+    //
+    private TextView showNameTextView;
+    private ListView serviceListView;
+    private String namestring;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_service);
+
+        bindWidget();
+
+        showView();
+    }// Main Method
+
+    private void showView() {
+        namestring = getIntent().getStringExtra("NameUser");
+        showNameTextView.setText(namestring);
+
     }
-}
+
+    private void bindWidget() {
+        showNameTextView = (TextView) findViewById(R.id.textView7);
+        serviceListView = (ListView) findViewById(R.id.listView);
+    }
+
+    public void clickReadOrder(View view) {
+
+    } // clicik
+
+
+} // Main class
