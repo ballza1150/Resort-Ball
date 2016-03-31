971a8c1ac222d67d5d513444ea64057736aef677
ballza1150 <ballzacup1@gmail.com>
2016-03-31 10:55:11 +0700

    can update to mySQL finish

--- app/src/main/java/nuttapon/ball/resortball/SingUpActiviity.java
+++ app/src/main/java/nuttapon/ball/resortball/SingUpActiviity.java
@@ -1,9 +1,21 @@
 package nuttapon.ball.resortball;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 
 public class SingUpActiviity extends AppCompatActivity {
 
@@ -45,18 +57,52 @@ public class SingUpActiviity extends AppCompatActivity {
 
         } else {
             // No Space
+            updateToMySQL();
         }
 
     } // ClickSing
+
+    private void updateToMySQL() {
+
+        // Connected Http
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try {
+
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("isAdd", "true"));
+            nameValuePairs.add(new BasicNameValuePair("User", userString));
+            nameValuePairs.add(new BasicNameValuePair("Password", passwordString));
+            nameValuePairs.add(new BasicNameValuePair("Name", nameString));
+            nameValuePairs.add(new BasicNameValuePair("IDcard", idcardString));
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://swiftcodingthai.com/ball/php_add_data_user.php");
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+            httpClient.execute(httpPost);
+
+            Toast.makeText(this, "บันทึกได้เรียบร้อย ขอบคุณครับ", Toast.LENGTH_SHORT).show();
+            finish();
+
+        } catch (Exception e) {
+            Toast.makeText(this, "ไม่สามารถบันทึกข้อมูลได้", Toast.LENGTH_SHORT).show();
+        }
+
+    } // update
 
     private boolean checkSpace() {
 
         boolean bolResult = true;
 
-        bolResult = userString.endsWith("") ||
+        bolResult = userString.equals("") ||
                 passwordString.equals("") ||
                 nameString.equals("") ||
                 idcardString.equals("");
+
+        Log.d("test", "bol=" + bolResult);
+
         return bolResult;
     }
 
